b2d172a5a7487e98d99f8782e7738a524ddcc20f
AnnapooraniBabu <annapoorani.babumanikandan@fwsa.freshworks.com>
2022-07-13 14:33:42 +0530

    Worked on login and register pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'users/login'
   get 'moms_delishes/index'
+  get 'moms_delishes/register'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
 
