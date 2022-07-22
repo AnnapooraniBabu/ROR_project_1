6a25d3e9bf51e58c85f75aed425ca73a8deb1cfd
AnnapooraniBabu <annapoorani.babumanikandan@fwsa.freshworks.com>
2022-07-22 17:32:50 +0530

    Working on validation of signup feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
     has_secure_password
-    validates :email, presence: true, formate: {with:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  , message: 'Email must be valid'}
+    validates :name , :email, :password, :number, presence: true
+    validates :name, length: { minimum: 3 }
+    validates :email,formate: {with:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  , message: 'Email must be valid'}
+    validates :password, length: { in: 6..10 }
+    validates :number, numericality: { equal_to: 10}
 end
 # /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/
