4cb63cd9f6195ce0a3fbe824f4ec0bca1e6f1b39
AnnapooraniBabu <annapoorani.babumanikandan@fwsa.freshworks.com>
2022-08-19 12:35:38 +0530

    Worked validation of add item form

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,4 +5,9 @@ class Item < ApplicationRecord
   has_one :cart  
 # :dependent  :destroy
   # foreign_key: {on_update: :cascade, ondelete: :cascade }
+  validates :item_name , presence: true, uniqueness: true
+  validates :img_url , presence: true, uniqueness: true
+  validates :category , presence: true, uniqueness: true
+  validates :quantity , presence: true, uniqueness: true
+  validates :price , presence: true, uniqueness: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
   validates :name , presence: true
   validates :email, :password_digest, :number, presence: true, uniqueness: true
   # validates :name, length: { minimum: 3, maximum: 15 }
-  # validates :email,formate: {with:  /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i  , message: 'Email must be valid', on: :create}
+  # validates :password, :format => {:with => /^[([a-z]|[A-Z])0-9_-]{6,40}$/, message: "must be at least 6 characters and include one number and one letter."}
+  # validates :email,format: {:with =>  \A[A-Z0-9+_.-]+@[A-Z0-9.-]+\Z  , message: 'The email can contain both alphabets eg:mike@gmail.com', on: :create}
+  # validates :email, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i , message: 'The email can contain both alphabets eg:mike@gmail.com', on: :create }
   # validates :password, length: { in: 6..10 }
   # validates :number, numericality: { equal_to: 10}
   # validates_presence_of :password, :on =>create
